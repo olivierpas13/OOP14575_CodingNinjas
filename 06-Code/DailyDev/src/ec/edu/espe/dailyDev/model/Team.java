0e5efaec708a5ed5a4593823cef9614ea6fc5fec
Olivier Paspuel <olivier_paspuel@hotmail.com>
2023-12-12 20:24:07 -0500

    Team V1.1.0

--- 06-Code/DailyDev/src/ec/edu/espe/dailyDev/model/Team.java
+++ 06-Code/DailyDev/src/ec/edu/espe/dailyDev/model/Team.java
@@ -1,5 +1,9 @@
 package ec.edu.espe.dailyDev.model;
 
+import com.google.gson.reflect.TypeToken;
+import ec.edu.espe.dailyDev.utils.FileHandler;
+import java.util.ArrayList;
+
 /**
  *
  * @author CodingNinjas 
@@ -7,4 +11,11 @@ package ec.edu.espe.dailyDev.model;
 public class Team {
     User[] users;
     String name;
+    
+        public static ArrayList<Team> getFromFile() {
+        return FileHandler.readFile("artists.json", new TypeToken<ArrayList<Team>>() {
+        }.getType());
+    }
+
+    
 }
